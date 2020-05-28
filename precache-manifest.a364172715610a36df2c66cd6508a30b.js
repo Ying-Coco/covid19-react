515b4c912eb8b5bc32efc6a5f9b36c2006b38b57
Ying <ywen1306@csu.fullerton.edu>
2020-05-27 21:05:32 -0700

    Updates

---
+++ precache-manifest.a364172715610a36df2c66cd6508a30b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29db0057e5f50571b743ec918963a6b1",
+    "url": "/covid19-react/index.html"
+  },
+  {
+    "revision": "3d15a04f291d904dec86",
+    "url": "/covid19-react/static/css/2.74d3dc50.chunk.css"
+  },
+  {
+    "revision": "627d0be18fc311a90677",
+    "url": "/covid19-react/static/css/main.df4495ed.chunk.css"
+  },
+  {
+    "revision": "3d15a04f291d904dec86",
+    "url": "/covid19-react/static/js/2.e6a06d8c.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid19-react/static/js/2.e6a06d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627d0be18fc311a90677",
+    "url": "/covid19-react/static/js/main.53b87c8c.chunk.js"
+  },
+  {
+    "revision": "eed4a9d781fc100735ba",
+    "url": "/covid19-react/static/js/runtime-main.fda9ee3e.js"
+  }
+]);
